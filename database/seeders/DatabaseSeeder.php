bf2b80922057866378cc3c794e58dcab1197a9ef
Riski <reesky.it@gmail.com>
2026-08-07 17:18:43 +0700

    feat(auth): create seeder for super admin and config R2 storage

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+         User::factory()->create([
+            'name' => 'Super Administrator',
+            'email' => 'admin@penerbitan.com',
+            'password' => bcrypt('password123'),
+            'role' => 'ADMIN',
         ]);
     }
 }
