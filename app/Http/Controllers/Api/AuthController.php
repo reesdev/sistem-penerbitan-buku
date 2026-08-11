402ce3da7e1cd36cd538453c1d439cd68c6fa03e
Riski <reesky.it@gmail.com>
2026-08-11 13:41:09 +0700

    feat(api): create register endpoint for authors

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,9 +3,37 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        // 1. Cek apakah data yang dikirim valid
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        // 2. Buat akun baru (Otomatis diberi role USER/Penulis)
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => 'USER',
+        ]);
+
+        // 3. Buatkan Token API sebagai tanda pengenal
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        // 4. Kembalikan balasan sukses
+        return response()->json([
+            'data' => $user,
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ], 201);
+    }
 }
